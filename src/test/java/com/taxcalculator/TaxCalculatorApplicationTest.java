81fe9adceecabb27a2868b721ff79affb6fb8214
ankitprahladsoni <ankitprahladsoni@gmail.com>
2018-08-27 00:27:22 -0400

    Added Junit 5

--- src/test/java/com/taxcalculator/TaxCalculatorApplicationTest.java
+++ src/test/java/com/taxcalculator/TaxCalculatorApplicationTest.java
@@ -1,52 +1,51 @@
 package com.taxcalculator;
 
-import org.junit.Assert;
-import org.junit.Test;
-
 import com.taxcalculator.receipt.Receipt;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 /**
  * Test class for {@link TaxCalculatorApplication}
- * 
- * @author ankit
  *
+ * @author ankit
  */
 public class TaxCalculatorApplicationTest {
 
-	private TaxCalculatorApplication taxCalculatorApplication = new TaxCalculatorApplication();
+    private TaxCalculatorApplication taxCalculatorApplication = new TaxCalculatorApplication();
 
-	@Test
-	public void test() {
+    @Test
+    public void test() {
 
-		String[] inputs = { "1 book at 12.49", "1 music CD at 14.99", "1 chocolate bar at 0.85" };
+        String[] inputs = {"1 book at 12.49", "1 music CD at 14.99", "1 chocolate bar at 0.85"};
 
-		Receipt receipt = taxCalculatorApplication.generateReceipt(inputs);
-		Assert.assertEquals(1.50, receipt.getTotalSalesTax(), 0.0);
-		Assert.assertEquals(29.83, receipt.getTotalAmount(), 0.0);
-		System.out.println(receipt);
-	}
+        Receipt receipt = taxCalculatorApplication.generateReceipt(inputs);
 
-	@Test
-	public void test2() {
+        Assertions.assertEquals(1.50, receipt.getTotalSalesTax());
+        Assertions.assertEquals(29.83, receipt.getTotalAmount());
+        System.out.println(receipt);
+    }
 
-		String[] inputs = { "1 imported box of chocolates at 10.00", "1 imported bottle of perfume at 47.50" };
+    @Test
+    public void test2() {
 
-		Receipt receipt = new TaxCalculatorApplication().generateReceipt(inputs);
-		Assert.assertEquals(7.65, receipt.getTotalSalesTax(), 0.0);
-		Assert.assertEquals(65.15, receipt.getTotalAmount(), 0.0);
-		System.out.println(receipt);
-	}
+        String[] inputs = {"1 imported box of chocolates at 10.00", "1 imported bottle of perfume at 47.50"};
 
-	@Test
-	public void test3() {
+        Receipt receipt = new TaxCalculatorApplication().generateReceipt(inputs);
+        Assertions.assertEquals(7.65, receipt.getTotalSalesTax());
+        Assertions.assertEquals(65.15, receipt.getTotalAmount());
+        System.out.println(receipt);
+    }
 
-		String[] inputs = { "1 imported bottle of perfume at 27.99", "1 bottle of perfume at 18.99",
-				"1 packet of headache pills at 9.75", "1 box of imported chocolates at 11.25" };
+    @Test
+    public void test3() {
 
-		Receipt receipt = new TaxCalculatorApplication().generateReceipt(inputs);
-		Assert.assertEquals(6.65, receipt.getTotalSalesTax(), 0.0);
-		Assert.assertEquals(74.63, receipt.getTotalAmount(), 0.0);
-		System.out.println(receipt);
+        String[] inputs = {"1 imported bottle of perfume at 27.99", "1 bottle of perfume at 18.99",
+                "1 packet of headache pills at 9.75", "1 box of imported chocolates at 11.25"};
 
-	}
+        Receipt receipt = new TaxCalculatorApplication().generateReceipt(inputs);
+        Assertions.assertEquals(6.70, receipt.getTotalSalesTax());
+        Assertions.assertEquals(74.63, receipt.getTotalAmount());
+        System.out.println(receipt);
+
+    }
 }
